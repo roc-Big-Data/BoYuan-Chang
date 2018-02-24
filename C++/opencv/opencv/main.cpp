01c6e2ccf876d81b7712d2b23a19fd0461304ee4
changboyuan <changboyuan@live.com>
2018-02-24 16:30:04 +0800

    opencv

--- C++/opencv/opencv/main.cpp
+++ C++/opencv/opencv/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>  
 #include <opencv2/core/core.hpp>  
-#include <opencv2/highgui/highgui.hpp>  
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
 
 using namespace cv;
 
 int main() {
 	// 读入一张图片（游戏原画）    
 	Mat img = imread("pic.jpg");
-	// 创建一个名为 "游戏原画"窗口    
-	namedWindow("游戏原画");
-	// 在窗口中显示游戏原画    
-	imshow("游戏原画", img);
-	// 等待6000 ms后窗口自动关闭    
-	waitKey(6000);
+	Mat gimg;
+	cvtColor(img, gimg, CV_BGR2GRAY);
+	imwrite("Gray_Image.jpg", gimg);
+	// 创建一个名为 "游戏原画"窗口  
+	Mat result;
+	Canny(img, result, 15, 45);
+	imwrite("Res.jpg", result);
+	return 0;
+
 }
